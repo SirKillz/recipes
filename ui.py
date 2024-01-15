746db0a65efd0b1024621a7c8868360f2e4e2155
SirKillz <sirkillz1198@gmail.com>
2024-01-15 08:58:02 -0600

    Added callback functionality to allow for the BrowserDriver class to interact with the UI class.

--- ui.py
+++ ui.py
@@ -1,13 +1,15 @@
+import tkinter.messagebox
 from tkinter import *
 from buttons import RecipeButton
+from tkinter import messagebox
 
 
 class UI(Tk):
     def __init__(self, driver):
         super().__init__()
-        self.stock_report()
         self.driver = driver
-        # self.login()
+
+        self.out_of_stock = []
 
         # Button Creation
         chili_button = RecipeButton(self, "chili")
@@ -19,11 +21,20 @@ class UI(Tk):
         chicken_wildrice_button = RecipeButton(self, 'chickenWildriceSoup')
         chicken_wildrice_button.pack()
 
+        notice_button = Button(text="Test", command=self.stock_report_notice)
+        notice_button.pack()
+
+
         self.mainloop()
 
-    def stock_report(self):
-        with open("stock_report.txt", mode="w") as file:
-            file.write("Automated Stock Report")
+    def stock_report_notice(self, oos_list):
+        if len(oos_list) == 0:
+            notice = "All items were in stock, and there were no substitutes."
+        else:
+            stock_string = "\n".join(oos_list)
+            notice = f"Out of Stock:\n{stock_string}"
+            tkinter.messagebox.showinfo(title="Stock Report", message=notice)
+
 
     def append_stock_report(self, item_name):
         with open("stock_report.txt", mode="a") as file:
